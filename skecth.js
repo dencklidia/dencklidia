54eb3f48040a94cb10225b1e39e33ef8fef6125f
dencklidia <lidia.pontarollo@escola.pr.gov.br>
2024-08-05 16:19:47 -0300

    Update skecth.js

--- skecth.js
+++ skecth.js
@@ -1,7 +1,18 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+  background("purple");
+}
+
+function draw() {
+
+  stroke("orange");
+  fill("yellow");
+
+  //console.log(mouseIsPressed);
+
+  if(mouseIsPressed) {
+   rect(mouseX, mouseY, 20, 35);
   }
   
-  function draw() {
-    background(220);
-  }
+
+}
